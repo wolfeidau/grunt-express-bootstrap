f4080e75f0823797a7321dac5450c447c5830900
Mark Wolfe <mark@wolfe.id.au>
2013-01-16 15:13:21 +1100

    More semi colon cleanup and removed some redundant code.

--- tasks/init/grunt-express-bootstrap/root/app.js
+++ tasks/init/grunt-express-bootstrap/root/app.js
@@ -2,10 +2,11 @@ var express = require('express')
 var configurations = module.exports
 var app = express()
 var nconf = require('nconf')
-var settings = require('./settings')(app, configurations, express)
 
-app.helpers(require('connect-cachify'))
+// load the settings
+require('./settings')(app, configurations, express)
 
+// merge nconf overrides with the configuration file.
 nconf.argv().env().file({ file: 'local.json' })
 
 // Routes
